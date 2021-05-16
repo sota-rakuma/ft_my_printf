471eca7af5f3467f0b6bc7b014de0a31f50ed1a2
srakuma <robben2159@gmail.com>
2021-05-16 18:34:01 +0900

    refind

--- ft_proc_precision.c
+++ ft_proc_precision.c
@@ -6,7 +6,7 @@
 /*   By: srakuma <srakuma@student.42tokyo.jp>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/05/09 18:21:09 by srakuma           #+#    #+#             */
-/*   Updated: 2021/05/14 21:16:52 by srakuma          ###   ########.fr       */
+/*   Updated: 2021/05/16 18:30:36 by srakuma          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -33,25 +33,27 @@ static char	*ft_proc_num_prcn(char *str, t_format *f_spec)
 	return (str);
 }
 
-static char	*get_int(t_format *f_spec, long int num)
+static char	*ft_get_int(t_format *f_spec, va_list ap)
 {
 	char	*str;
 
-	if (num == 0 && f_spec->prcn == 0)
+	if (f_spec->specifier[3] || f_spec->specifier[4])
+		str = ft_itoa((int)va_arg(ap, int));
+	else if (f_spec->specifier[5])
+		str = ft_utoa((unsigned int)va_arg(ap, unsigned int));
+	else if (f_spec->specifier[2])
+		str = ft_xtoa((unsigned long int)va_arg(ap, long int), "abcdef");
+	else if (f_spec->specifier[6])
+		str = ft_xtoa((unsigned long int)va_arg(ap, unsigned int), "abcdef");
+	else
+		str = ft_xtoa((unsigned long int)va_arg(ap, unsigned int), "ABCDEF");
+	if (*str == '0' && f_spec->prcn == 0)
 	{
 		f_spec->flag[4] = 0;
 		if (f_spec->specifier[2])
 			return (ft_strdup("0x"));
 		return (ft_strdup(""));
 	}
-	else if (f_spec->specifier[3] || f_spec->specifier[4])
-		str = ft_itoa((int)num);
-	else if (f_spec->specifier[5])
-		str = ft_utoa((unsigned int)num);
-	else if (f_spec->specifier[2] || f_spec->specifier[6])
-		str = ft_xtoa(num, "abcdef");
-	else
-		str = ft_xtoa(num, "ABCDEF");
 	str = ft_proc_num_prcn(str, f_spec);
 	return (str);
 }
@@ -94,6 +96,6 @@ char	*ft_get_str_with_prcn(va_list ap, t_format *f_spec)
 	else if (f_spec->specifier[0] || f_spec->specifier[1])
 		str = ft_proc_str_prcn(ap, f_spec);
 	else
-		str = get_int(f_spec, (long int)va_arg(ap, long int));
+		str = ft_get_int(f_spec, ap);
 	return (str);
 }
